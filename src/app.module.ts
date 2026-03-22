96eb0b1ead7595207d426ec8066658cd73ba263d
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-03-22 12:32:39 -0300

    feat: add typeorm settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlertModule } from './modules/alert/alert.module';
 
 @Module({
-  imports: [AlertModule],
+  imports: [
+    // npm i @nestjs/config
+    ConfigModule.forRoot({ isGlobal: true }),
+    // npm i @nestjs/typeorm typeorm mysql2
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow<string>('DATABASE_HOST'),
+        port: configService.getOrThrow<number>('DATABASE_PORT'),
+        username: configService.getOrThrow<string>('DATABASE_USERNAME'),
+        password: configService.getOrThrow<string>('DATABAE_PASSWORD'),
+        database: configService.getOrThrow<string>('DATABASE_NAME'),
+        entities: [],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    AlertModule,
+  ],
   controllers: [],
   providers: [],
 })
