4f1fb11551b5d0176c358beaf8dd4758683b6068
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-03-25 07:26:18 -0300

    docs: enable swagger documentation

---
+++ src/common/swagger/setup.swagger.ts
@@ -0,0 +1,26 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
+
+// npm i @nestjs/swagger swagger-ui-express swagger-themes
+
+export function setupSwagger(app: INestApplication): void {
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Stock Price Alert API')
+      .setDescription(
+        'A NestJS stock price alert API. It allows users to set alerts for specific tickers and leverages BullMQ queues for efficient background processing, triggering email notifications when target prices are reached.',
+      )
+      .setVersion('1.0')
+      .build(),
+  );
+  const theme = new SwaggerTheme();
+  SwaggerModule.setup('/api', app, document, {
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './common/swagger/setup.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
