bfbb3039951195c4f9dded76d7677b3f349cd2ab
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-04-02 17:46:54 -0300

    test: add create test for alert controller

--- src/modules/alert/tests/alert.controller.spec.ts
+++ src/modules/alert/tests/alert.controller.spec.ts
@@ -1,15 +1,19 @@
 import { NotFoundException } from '@nestjs/common';
+import { describe } from 'node:test';
+import { CreatedResponseDto } from 'src/common/dtos/created-response.dto';
 import { makeUuidDto } from 'src/common/test/factories/uuid.dto.factory';
 import { AlertController } from '../alert.controller';
 import { AlertService } from '../alert.service';
 import { AlertResponseDto } from '../dtos/alert-response.dto';
 import { makeAlertEntity } from './factories/alert.entity.factory';
+import { makeCreateAlertDto } from './factories/create-alert.dto.factory';
 
 describe('AlertController', () => {
   let alertController: AlertController;
   const alertServiceMock = {
     findAll: jest.fn(),
     findOne: jest.fn(),
+    create: jest.fn(),
   };
 
   beforeEach(() => {
@@ -55,6 +59,16 @@ describe('AlertController', () => {
         NotFoundException,
       );
       expect(alertServiceMock.findOne).toHaveBeenCalledWith(dto.id);
+    });
+  });
+
+  describe('create', () => {
+    it('should return a created response', async () => {
+      const dto = makeCreateAlertDto();
+      alertServiceMock.create.mockResolvedValue(makeAlertEntity());
+      const response = await alertController.create(dto);
+      expect(alertServiceMock.create).toHaveBeenCalledWith(dto);
+      expect(response instanceof CreatedResponseDto).toBe(true);
     });
   });
 });
