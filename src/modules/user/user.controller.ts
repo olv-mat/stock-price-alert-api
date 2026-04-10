9d0ec94ffbb264e5bb749147cd9de6b034a4aff3
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-04-10 17:32:15 -0300

    docs: document users find one endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { UuidDto } from 'src/common/dtos/uuid.dto';
 import {
   SwaggerInternalServerError,
+  SwaggerNotFound,
   SwaggerOperation,
 } from 'src/common/swagger/decorators.swagger';
 import { UserResponseDto } from './dtos/user-response.dto';
@@ -20,6 +21,9 @@ export class UserController {
   }
 
   @Get(':id')
+  @SwaggerOperation('Retrieve a specific user')
+  @SwaggerNotFound('User not found')
+  @SwaggerInternalServerError()
   public async findOne(@Param() { id }: UuidDto): Promise<UserResponseDto> {
     const userEntity = await this.userService.findOne(id);
     return UserResponseDto.fromEntity(userEntity);
