b40150fbb2fbf9c3f9903f59ddc5741d0ad37941
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-04-26 12:31:52 -0300

    test: add register tests for auth controller

--- src/modules/auth/tests/auth.controller.spec.ts
+++ src/modules/auth/tests/auth.controller.spec.ts
@@ -1,13 +1,16 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
+import { makeCreateUserDto } from 'src/modules/user/tests/factories/create-user.dto.factory';
+import { makeUserEntity } from 'src/modules/user/tests/factories/user.entity.factory';
 import { AuthController } from '../auth.controller';
 import { AuthService } from '../auth.service';
-import { LoginResponseDto } from '../dtos/login-response.dto';
+import { AuthResponseDto } from '../dtos/auth-response.dto';
 import { makeLoginDto } from './factories/login.dto.factory';
 
 describe('AuthController', () => {
   let authController: AuthController;
   const authServiceMock = {
     login: jest.fn(),
+    register: jest.fn(),
   };
 
   beforeEach(() => {
@@ -18,12 +21,14 @@ describe('AuthController', () => {
   });
 
   describe('login', () => {
-    it('should return a login response', async () => {
+    it('should correctly call the service and return an auth response', async () => {
       const dto = makeLoginDto();
-      authServiceMock.login.mockResolvedValue('');
+      const userEntity = makeUserEntity();
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+      authServiceMock.login.mockResolvedValue({ userEntity, token });
       const response = await authController.login(dto);
       expect(authServiceMock.login).toHaveBeenCalledWith(dto);
-      expect(response instanceof LoginResponseDto).toBe(true);
+      expect(response instanceof AuthResponseDto).toBe(true);
     });
 
     it('should propagate service exceptions', async () => {
@@ -33,6 +38,26 @@ describe('AuthController', () => {
         UnauthorizedException,
       );
       expect(authServiceMock.login).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('register', () => {
+    it('should correctly call the service and return an auth response', async () => {
+      const dto = makeCreateUserDto();
+      const userEntity = makeUserEntity();
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+      authServiceMock.register.mockResolvedValue({ userEntity, token });
+      const response = await authController.register(dto);
+      expect(authServiceMock.register).toHaveBeenCalledWith(dto);
+      expect(response instanceof AuthResponseDto).toBe(true);
+    });
+
+    it('should propagate service exceptions', async () => {
+      const dto = makeCreateUserDto();
+      authServiceMock.register.mockRejectedValue(new ConflictException());
+      await expect(authController.register(dto)).rejects.toThrow(
+        ConflictException,
+      );
     });
   });
 });
