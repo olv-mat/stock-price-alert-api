46c4ec74db58987a8c961a504192d2f47f9083b8
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-04-29 07:53:21 -0300

    fix: adjust alert service tests

--- src/modules/alert/tests/alert.service.spec.ts
+++ src/modules/alert/tests/alert.service.spec.ts
@@ -1,6 +1,7 @@
 import { NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { makeAccessTokenPayload } from 'src/common/test/factories/access-token-payload.factory';
 import { makeUuidDto } from 'src/common/test/factories/uuid.dto.factory';
 import { Repository } from 'typeorm';
 import { AlertService } from '../alert.service';
@@ -9,8 +10,8 @@ import { makeAlertEntity } from './factories/alert.entity.factory';
 import { makeCreateAlertDto } from './factories/create-alert.dto.factory';
 
 type AlertServiceContext = {
-  alertRepositoryMock: Repository<AlertEntity>;
   alertService: AlertService;
+  alertRepositoryMock: Repository<AlertEntity>;
 };
 
 describe('AlertService', () => {
@@ -19,33 +20,34 @@ describe('AlertService', () => {
     jest.clearAllMocks();
     const module = await Test.createTestingModule({
       providers: [
+        AlertService,
         {
           provide: getRepositoryToken(AlertEntity),
           useValue: {
             find: jest.fn(),
-            findOneBy: jest.fn(),
+            findOne: jest.fn(),
             save: jest.fn(),
             delete: jest.fn(),
           },
         },
-        AlertService,
       ],
     }).compile();
     context = {
-      alertRepositoryMock: module.get(getRepositoryToken(AlertEntity)),
       alertService: module.get(AlertService),
+      alertRepositoryMock: module.get(getRepositoryToken(AlertEntity)),
     };
   });
 
   describe('findAll', () => {
     it('should return a list of alerts', async () => {
       const { alertRepositoryMock, alertService } = context;
+      const { sub } = makeAccessTokenPayload();
       const alertEntities = [makeAlertEntity()];
       const spy = jest
         .spyOn(alertRepositoryMock, 'find')
         .mockResolvedValue(alertEntities);
-      const response = await alertService.findAll();
-      expect(spy).toHaveBeenCalled();
+      const response = await alertService.findAll(sub);
+      expect(spy).toHaveBeenCalledWith({ where: { user: { id: sub } } });
       expect(response).toEqual(alertEntities);
     });
   });
@@ -53,37 +55,49 @@ describe('AlertService', () => {
   describe('findOne', () => {
     it('should return a alert', async () => {
       const { alertRepositoryMock, alertService } = context;
+      const { sub } = makeAccessTokenPayload();
       const { id } = makeUuidDto();
       const alertEntity = makeAlertEntity();
       const spy = jest
-        .spyOn(alertRepositoryMock, 'findOneBy')
+        .spyOn(alertRepositoryMock, 'findOne')
         .mockResolvedValue(alertEntity);
-      const response = await alertService.findOne(id);
-      expect(spy).toHaveBeenCalledWith({ id: id });
+      const response = await alertService.findOne(sub, id);
+      expect(spy).toHaveBeenCalledWith({
+        where: { id: id, user: { id: sub } },
+      });
       expect(response).toEqual(alertEntity);
     });
 
     it('should throw a not found excpetion when alert does not exist', async () => {
       const { alertRepositoryMock, alertService } = context;
+      const { sub } = makeAccessTokenPayload();
       const { id } = makeUuidDto();
       const spy = jest
-        .spyOn(alertRepositoryMock, 'findOneBy')
+        .spyOn(alertRepositoryMock, 'findOne')
         .mockResolvedValue(null);
-      await expect(alertService.findOne(id)).rejects.toThrow(NotFoundException);
-      expect(spy).toHaveBeenCalledWith({ id: id });
+      await expect(alertService.findOne(sub, id)).rejects.toThrow(
+        NotFoundException,
+      );
+      expect(spy).toHaveBeenCalledWith({
+        where: { id: id, user: { id: sub } },
+      });
     });
   });
 
   describe('create', () => {
     it('should create a new alert', async () => {
       const { alertRepositoryMock, alertService } = context;
+      const { sub } = makeAccessTokenPayload();
       const dto = makeCreateAlertDto();
       const alertEntity = makeAlertEntity();
       const spy = jest
         .spyOn(alertRepositoryMock, 'save')
         .mockResolvedValue(alertEntity);
-      const response = await alertService.create(dto);
-      expect(spy).toHaveBeenCalledWith(dto);
+      const response = await alertService.create(sub, dto);
+      expect(spy).toHaveBeenCalledWith({
+        ...dto,
+        user: { id: sub },
+      });
       expect(response).toEqual(alertEntity);
     });
   });
@@ -91,25 +105,33 @@ describe('AlertService', () => {
   describe('delete', () => {
     it('should delete a alert', async () => {
       const { alertRepositoryMock, alertService } = context;
-      const alertEntity = makeAlertEntity();
+      const { sub } = makeAccessTokenPayload();
       const { id } = makeUuidDto();
-      const findOneBySpy = jest
-        .spyOn(alertRepositoryMock, 'findOneBy')
-        .mockResolvedValue(alertEntity);
-      const deleteSpy = jest.spyOn(alertRepositoryMock, 'delete');
-      await alertService.delete(id);
-      expect(findOneBySpy).toHaveBeenCalledWith({ id: id });
-      expect(deleteSpy).toHaveBeenCalledWith(alertEntity.id);
+      const alertEntity = makeAlertEntity();
+      const spies = {
+        findOne: jest
+          .spyOn(alertRepositoryMock, 'findOne')
+          .mockResolvedValue(alertEntity),
+        delete: jest.spyOn(alertRepositoryMock, 'delete'),
+      };
+      await alertService.delete(sub, id);
+      expect(spies.findOne).toHaveBeenCalledWith({
+        where: { id: id, user: { id: sub } },
+      });
+      expect(spies.delete).toHaveBeenCalledWith(alertEntity.id);
     });
 
     it('should throw a not found exception when alert does not exist', async () => {
       const { alertRepositoryMock, alertService } = context;
+      const { sub } = makeAccessTokenPayload();
       const { id } = makeUuidDto({
         id: '27d00cd0-31c8-4630-9e4f-e2f890689a73',
       });
       const spy = jest.spyOn(alertRepositoryMock, 'delete');
-      jest.spyOn(alertRepositoryMock, 'findOneBy').mockResolvedValue(null);
-      await expect(alertService.delete(id)).rejects.toThrow(NotFoundException);
+      jest.spyOn(alertRepositoryMock, 'findOne').mockResolvedValue(null);
+      await expect(alertService.delete(sub, id)).rejects.toThrow(
+        NotFoundException,
+      );
       expect(spy).not.toHaveBeenCalled();
     });
   });
